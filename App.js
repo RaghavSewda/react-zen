e198786c835a5532d6e42096a15a4df519b1b2c2
R.sewda <sewdaraghav@gmail.com>
2024-07-10 10:51:56 +0530

    episode-2

--- App.js
+++ App.js
@@ -4,12 +4,15 @@
 //   "Hello from React"
 // );
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm a h1 tag"),
-    React.createElement("h2", {}, "I'm a h2 tag"),
+    React.createElement("h1", {}, "h1 tag"),
+    React.createElement("h2", {}, "h2 tag"),
   ])
 );
 
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste react
+
+# Parcel (parcel documentation)
+
+- Dev build
+- Local server
+- HMR - hot module replacement
+- File Watching Algorithms - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differntial Bundling - support older versions
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
