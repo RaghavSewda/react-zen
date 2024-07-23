47f370031d5b24bee3402e43f47cdf320ea00ddb
R.sewda <sewdaraghav@gmail.com>
2024-07-23 06:53:44 +0530

    episode-5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,25 @@
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+  const [List, setList] = useState(resList);
   return (
     <div className="body">
       <div className="search-bar">
         <input type="text" placeholder="Search"></input>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterList = List.filter((res) => res.avgRating > 4.5);
+            setList(filterList);
+          }}
+        >
+          Top Rated
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {List.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
