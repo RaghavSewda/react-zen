6a37f95eeeab963762a009c9835b8ffd58dce1a3
R.sewda <sewdaraghav@gmail.com>
2024-07-26 07:10:31 +0530

    assignment-5

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,12 +2,37 @@ import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
 
+function filterData(searchRes, List) {
+  return List.filter((res) =>
+    res.name.toLowerCase().includes(searchRes.toLowerCase())
+  );
+}
 const Body = () => {
   const [List, setList] = useState(resList);
+  const [searchRes, setsearchRes] = useState("");
   return (
     <div className="body">
-      <div className="search-bar">
-        <input type="text" placeholder="Search"></input>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder=""
+          value={searchRes}
+          onChange={(e) => {
+            setsearchRes(e.target.value);
+            if (e.target.value === "") {
+              setList(resList);
+            }
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchRes, List);
+            setList(data);
+          }}
+        >
+          Search
+        </button>
         <button
           className="filter-btn"
           onClick={() => {
